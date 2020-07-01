c27c7f5038c98e9800a51e682c5e22c75bd57c26
Abdullah Zubair <abdullah2020.lhr@gmail.com>
2020-07-02 00:16:39 +0500

    Convetrs Your age into dogs age

---
+++ index.js
@@ -0,0 +1,16 @@
+//store your age
+const myAge = 20;
+//store 2 early Years of dog
+let earlyYears = 2;
+earlyYears *= 10.5;
+//substact 2 years from your age and store as Later Years
+let laterYears = myAge - 2;
+//Multiply your later years with 4 as its equal dogs later years
+laterYears *= 4;
+//store my age as dog years
+myAgeInDogsYears = earlyYears + laterYears;
+//store your name in lowercase
+let myName = 'Abdullah'.toLowerCase();
+//Prints on console
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogsYears} years old in dog years.`);
+
